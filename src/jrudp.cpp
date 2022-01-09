dcaa1bdcb8637f226ca1ca7aea0e471aa4256e28
B777B2056 <1937343022@qq.com>
2022-01-09 18:02:33 +0800

    20220109:Fast retransmit

--- src/jrudp.cpp
+++ src/jrudp.cpp
@@ -4,8 +4,8 @@ std::string jrReliableUDP::error_msg(std::string msg) {
     return msg + ":" + strerror(errno);
 }
 
-jrReliableUDP::Socket::Socket() : current_seq_num(init_seq_num()), current_ack_num(0),
-                                  current_win_size(flow_win_size()), current_state(CLOSED) {
+jrReliableUDP::Socket::Socket() : cur_seq_num(init_seq_num()), cur_ack_num(0),
+                                  cur_win_size(flow_win_size()), cur_state(CLOSED), dupack_cnt(0) {
     sockfd = ::socket(AF_INET, SOCK_DGRAM, 0);
     if(-1 == sockfd) {
         throw std::runtime_error(error_msg("Socket create failed"));
@@ -15,7 +15,13 @@ jrReliableUDP::Socket::Socket() : current_seq_num(init_seq_num()), current_ack_n
 jrReliableUDP::Socket::Socket(int fd, bool is_passive_end, sockaddr_in peer_addr,
                               uint32_t csn, uint32_t can, ConnectionState cs)
     : sockfd(fd), is_passive_end(is_passive_end), addr(peer_addr),
-      current_seq_num(csn), current_ack_num(can), current_win_size(flow_win_size()), current_state(cs) {}
+      cur_seq_num(csn), cur_ack_num(can), cur_win_size(flow_win_size()), cur_state(cs), dupack_cnt(0) {}
+
+jrReliableUDP::Socket::~Socket() {
+    if(cur_state == LISTEN) {
+        ::close(sockfd);
+    }
+}
 
 uint32_t jrReliableUDP::Socket::init_seq_num() const {
     return 0;
@@ -25,14 +31,21 @@ uint16_t jrReliableUDP::Socket::flow_win_size() const {
     return 8;
 }
 
-void jrReliableUDP::Socket::set_local_address(uint port) {
+void jrReliableUDP::Socket::set_timeout(uint ms) {
+  timeval tv;
+  tv.tv_sec = 0;
+  tv.tv_usec = ms*1000;
+  ::setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
+}
+
+void jrReliableUDP::Socket::set_local_address(uint16_t port) {
     ::memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = htonl(INADDR_ANY);
     addr.sin_port = htons(port);
 }
 
-void jrReliableUDP::Socket::set_peer_address(std::string ip, uint port) {
+void jrReliableUDP::Socket::set_peer_address(std::string ip, uint16_t port) {
     // find host ip by name through DNS service
     auto hpk = ::gethostbyname(ip.c_str());
     if(!hpk) {
@@ -42,61 +55,100 @@ void jrReliableUDP::Socket::set_peer_address(std::string ip, uint port) {
     ::memset(&addr, 0, sizeof(addr));
     // fill the struct
     addr.sin_family = AF_INET;		// protocol
-    addr.sin_addr.s_addr = inet_addr(inet_ntoa(*(in_addr*)(hpk->h_addr_list[0])));
+    addr.sin_addr.s_addr = inet_addr(inet_ntoa(*reinterpret_cast<in_addr*>(hpk->h_addr_list[0])));
     addr.sin_port = htons(port);		// target process port number
 }
 
-void jrReliableUDP::Socket::send_raw_packet(uint type) {
-    RawPacket p1;
-    p1.seq_num = current_seq_num;
-    p1.ack_num = current_ack_num;
-    p1.win_size = current_win_size;
-    p1.type = type;
-    p1.mss = DEFAULT_MSS;
+void jrReliableUDP::Socket::send_raw_packet(const RawPacket& pkg) {
     char buf[sizeof(RawPacket)];
-    ::memmove(buf, &p1, sizeof(RawPacket));
-    ::sendto(sockfd, buf, sizeof(RawPacket), 0, reinterpret_cast<sockaddr*>(&addr), sizeof(addr));
-    sent_packets[p1.seq_num] = p1;
-    if(type == ACK) {
-        return ;
+    ::memmove(buf, &pkg, sizeof(RawPacket));
+    if(-1 == ::sendto(sockfd, buf, sizeof(RawPacket), 0, reinterpret_cast<sockaddr*>(&addr), sizeof(addr))) {
+        throw std::runtime_error(error_msg("Send error:"));
     }
-    socklen_t len = sizeof(addr);
-    RawPacket p2;
+    if(sent_pkgs.find(pkg.seq_num) == sent_pkgs.end()) {
+        sent_pkgs[pkg.seq_num] = pkg;
+    }
+    if(pkg.type == ACK) { return ; }  // ACK doesn't need retransmit
+    // Wait ACK and Retransmit function
+    socklen_t len = sizeof(sockaddr_in);
+    RawPacket ack_pkg;
     ::memset(&addr, 0, len);
-    ++current_seq_num;
+    ++cur_seq_num;
     if(::recvfrom(sockfd, buf, sizeof(RawPacket), 0, reinterpret_cast<sockaddr*>(&addr), &len) > 0) {
-        ::memmove(&p2, buf, sizeof(RawPacket));
-        if(IS_ACK(p2.type)) {
-            if(p2.ack_num == current_seq_num) {
+        ::memmove(&ack_pkg, buf, sizeof(RawPacket));
+        if(IS_ACK(ack_pkg.type)) {
+            // ACK arrived
+            if(ack_pkg.ack_num >= cur_seq_num) {
                 // Correct ACK
-                current_ack_num = p2.seq_num + 1;
+                dupack_cnt = 0; // Reset counter
+                cur_ack_num = ack_pkg.seq_num + 1; // Update ACK num
+                sent_pkgs.erase(sent_pkgs.begin(), sent_pkgs.find(cur_ack_num));  // Update sent buffer
             } else {
-                // Wrong ACK, need retransmit
-                RawPacket p = sent_packets[p2.ack_num];
-                ::memmove(buf, &p, sizeof(RawPacket));
-                ::sendto(sockfd, buf, sizeof(RawPacket), 0, reinterpret_cast<sockaddr*>(&addr), sizeof(addr));
+                // Duplicate ACK
+                if(dupack_cnt == DUPTHRESH) {
+                    // Reset counter
+                    dupack_cnt = 0;
+                    // Retransmit all
+                    uint32_t right = cur_seq_num;
+                    cur_seq_num = ack_pkg.ack_num;
+                    for(uint32_t i = ack_pkg.ack_num; i <= right; ++i) {
+                        send_raw_packet(sent_pkgs[i]);
+                    }
+                } else {
+                    ++dupack_cnt;
+                }
             }
+        } else {
+            // RST arrived
+            ::close(sockfd);
+            throw std::runtime_error("Connection closed by peer.");
         }
     } else {
+        ::close(sockfd);
         throw std::runtime_error(error_msg("Send failed"));
     }
 }
 
+void jrReliableUDP::Socket::send_raw_packet(uint type) {
+    RawPacket pkg;
+    pkg.seq_num = cur_seq_num;
+    pkg.ack_num = cur_ack_num;
+    pkg.win_size = cur_win_size;
+    pkg.type = type;
+    pkg.mss = DEFAULT_MSS;
+    send_raw_packet(pkg);
+}
+
 jrReliableUDP::RawPacket jrReliableUDP::Socket::wait_raw_packet() {
     socklen_t len = sizeof(addr);
-    RawPacket packet;
-    char packet_buf[sizeof(RawPacket)];
+    RawPacket pkg;
+    char buf[sizeof(RawPacket)];
+PKG_MISSING:
     ::memset(&addr, 0, len);
-    if(::recvfrom(sockfd, packet_buf, sizeof(RawPacket), 0, reinterpret_cast<sockaddr*>(&addr), &len) > 0) {
-        ::memmove(&packet, packet_buf, sizeof(RawPacket));
-        current_ack_num = packet.seq_num + 1;
-        return packet;
+    if(::recvfrom(sockfd, buf, sizeof(RawPacket), 0, reinterpret_cast<sockaddr*>(&addr), &len) > 0) {
+        ::memmove(&pkg, buf, sizeof(RawPacket));
+        if(rcvd_pkgs.rbegin()->first == pkg.seq_num + 1) {
+            cur_ack_num = pkg.seq_num + 1;
+            rcvd_pkgs[pkg.seq_num] = pkg;
+            send_raw_packet(ACK);
+        } else {
+            send_raw_packet(ACK);
+            goto PKG_MISSING;
+        }
+        RawPacket ret = rcvd_pkgs.begin()->second;
+        rcvd_pkgs.erase(rcvd_pkgs.begin());
+        return ret;
     } else {
-        throw std::runtime_error(error_msg("Recv failed"));
+        if(errno == EAGAIN) {
+            // Recv timeout
+
+        } else {
+            throw std::runtime_error(error_msg("Recv failed"));
+        }
     }
 }
 
-void jrReliableUDP::Socket::bind(uint port) {
+void jrReliableUDP::Socket::bind(uint16_t port) {
     set_local_address(port);
     if(-1 == ::bind(sockfd, reinterpret_cast<sockaddr*>(&addr), sizeof(sockaddr_in))) {
         ::close(sockfd);
@@ -105,17 +157,18 @@ void jrReliableUDP::Socket::bind(uint port) {
     this->port = port;
 }
 
-void jrReliableUDP::Socket::connect(std::string peer_ip, uint peer_port) {
+void jrReliableUDP::Socket::connect(std::string peer_ip, uint16_t peer_port) {
+    set_timeout(RTT_INIT);
     is_passive_end = false;
     while(true) {
-        switch(current_state) {
+        switch(cur_state) {
         case CLOSED:
             // Set peer ip and port
             set_peer_address(peer_ip, peer_port);
             //    ::alarm(OVERTIME_SEC);
             // Send SYN and ISN(CLOSED->SYN_SENT)
             send_raw_packet(SYN);
-            current_state = SYN_SENT;
+            cur_state = SYN_SENT;
             break;
         case SYN_SENT:
             // wait for peer's SYN
@@ -123,13 +176,11 @@ void jrReliableUDP::Socket::connect(std::string peer_ip, uint peer_port) {
             RawPacket peer_syn = wait_raw_packet();
             // Received correct SYN(SYN_SENT->ESTABLISHED)
             if(IS_SYN(peer_syn.type)) {
-                current_state = ESTABLISHED;
+                cur_state = ESTABLISHED;
             }
         }
             break;
         case ESTABLISHED:
-            // Send ACK(ESTABLISHED)
-            send_raw_packet(ACK);
             return ;
         default:
             throw std::runtime_error("Connection established");
@@ -139,9 +190,9 @@ void jrReliableUDP::Socket::connect(std::string peer_ip, uint peer_port) {
 
 void jrReliableUDP::Socket::listen() {
     is_passive_end = true;
-    switch (current_state) {
+    switch (cur_state) {
     case CLOSED:
-        current_state = LISTEN;
+        cur_state = LISTEN;
         break;
     case LISTEN:
         throw std::runtime_error("Already listening");
@@ -153,7 +204,7 @@ void jrReliableUDP::Socket::listen() {
 jrReliableUDP::Socket jrReliableUDP::Socket::accept() {
     bool stop = false;
     while(!stop) {
-        switch(current_state) {
+        switch(cur_state) {
         case CLOSED:
             throw std::runtime_error("Not listening");
         case LISTEN:
@@ -161,7 +212,7 @@ jrReliableUDP::Socket jrReliableUDP::Socket::accept() {
             // wait for peer's SYN(LISTEN->SYN_RCVD)
             RawPacket syn = wait_raw_packet();
             if(IS_SYN(syn.type)) {
-                current_state = SYN_RCVD;
+                cur_state = SYN_RCVD;
             } else {
                 // Received other type packet, send RST
                 send_raw_packet(RST);
@@ -170,31 +221,30 @@ jrReliableUDP::Socket jrReliableUDP::Socket::accept() {
         }
             break;
         case SYN_RCVD:
-            send_raw_packet(ACK);
             // Send SYN and wait for peer's ACK
             send_raw_packet(SYN);
             // SYN_RCVD->ESTABLISHED
-            current_state = ESTABLISHED;
+            cur_state = ESTABLISHED;
             break;
         case ESTABLISHED:
-            current_state = LISTEN;
+            cur_state = LISTEN;
             stop = true;
             break;
         default:
             break;
         }
     }
-    return Socket(::dup(sockfd), true, addr, current_seq_num, current_ack_num, ESTABLISHED);
+    return Socket(::dup(sockfd), true, addr, cur_seq_num, cur_ack_num, ESTABLISHED);
 }
 
 void jrReliableUDP::Socket::disconnect() {
     if(is_passive_end) {
         // Server
         while(true) {
-            switch(current_state) {
+            switch(cur_state) {
             case ESTABLISHED:
                 // ESTABLISHED->CLOSE_WAIT
-                current_state = CLOSE_WAIT;
+                cur_state = CLOSE_WAIT;
                 break;
             case CLOSE_WAIT:
             {
@@ -202,15 +252,14 @@ void jrReliableUDP::Socket::disconnect() {
                 RawPacket fin = wait_raw_packet();
                 if(IS_FIN(fin.type)) {
                     // Recv the FIN, CLOSE_WAIT->LAST_ACK
-                    current_state = LAST_ACK;
+                    cur_state = LAST_ACK;
                 }
             }
                 break;
             case LAST_ACK:
                 // Send FIN to peer, wait peer's ACK, LAST_ACK->CLOSED
-                send_raw_packet(ACK);
                 send_raw_packet(FIN);
-                current_state = CLOSED;
+                cur_state = CLOSED;
                 break;
             case CLOSED:
                 ::close(sockfd);
@@ -222,25 +271,24 @@ void jrReliableUDP::Socket::disconnect() {
     } else {
         // Client
         while(true) {
-            switch(current_state) {
+            switch(cur_state) {
             case ESTABLISHED:
                 // ESTABLISHED->FIN_WAIT_1
-                current_state = FIN_WAIT;
+                cur_state = FIN_WAIT;
                 break;
             case FIN_WAIT:
                 // Send FIN to peer(FIN_WAIT_1) and wait for peer's ACK(FIN_WAIT_1->FIN_WAIT_2)
                 send_raw_packet(FIN);
                 // FIN_WAIT_2->TIME_WAIT
-                current_state = TIME_WAIT;
+                cur_state = TIME_WAIT;
                 break;
             case TIME_WAIT:
             {
                 // wait peer's FIN
                 RawPacket fin = wait_raw_packet();
                 if(IS_FIN(fin.type)) {
-                    send_raw_packet(ACK);
                     // TIME_WAIT->CLOSED
-                    current_state = CLOSED;
+                    cur_state = CLOSED;
                 }
             }
                 break;
@@ -252,4 +300,12 @@ void jrReliableUDP::Socket::disconnect() {
             }
         }
     }
+}
+
+std::string jrReliableUDP::Socket::recv_pkg() {
+
+}
+
+void jrReliableUDP::Socket::send_pkg(const std::string& data) {
+
 }
--- src/jrudp.hpp
+++ src/jrudp.hpp
@@ -19,6 +19,8 @@
 #define ACK (8)
 #define DEFAULT_MSS (1460)
 #define OVERTIME_SEC (1)
+#define DUPTHRESH 3
+#define RTT_INIT 1000
 
 #define IS_ACK(type) ((type&ACK) == ACK)
 #define IS_SYN(type) ((type&SYN) == SYN)
@@ -47,29 +49,38 @@ namespace jrReliableUDP {
         uint port;
         bool is_passive_end;
         sockaddr_in addr;
-        uint32_t current_seq_num;
-        uint32_t current_ack_num;
-        uint16_t current_win_size;
-        ConnectionState current_state;
-        std::map<uint32_t, RawPacket> sent_packets;
+        uint32_t cur_seq_num;
+        uint32_t cur_ack_num;
+        uint16_t cur_win_size;
+        ConnectionState cur_state;
+        int dupack_cnt; // Duplicate ACK counter
+        std::map<uint32_t, RawPacket> sent_pkgs, rcvd_pkgs;
 
     private:
         Socket(int fd, bool is_passive_end, sockaddr_in addr, uint32_t csn, uint32_t can, ConnectionState cs);
         uint32_t init_seq_num() const;
         uint16_t flow_win_size() const;
-        void set_local_address(uint port);
-        void set_peer_address(std::string ip, uint port);
+        void set_timeout(uint ms);
+        void set_local_address(uint16_t port);
+        void set_peer_address(std::string ip, uint16_t port);
+        void send_raw_packet(const RawPacket& pkg);
         void send_raw_packet(uint type);
         RawPacket wait_raw_packet();
 
     public:
         Socket();
-        ~Socket() = default;
-        void bind(uint port);   // Bind a local port
-        void connect(std::string peer_ip, uint peer_port);  // Actively open, Send SYN and ISN to peer, CLOSED->SYN_SENT
+        Socket(const Socket&) = default;
+        Socket(Socket&&) = default;
+        ~Socket();
+        Socket& operator=(const Socket&) = default;
+        Socket& operator=(Socket&&) = default;
+        void bind(uint16_t port);   // Bind a local port
+        void connect(std::string peer_ip, uint16_t peer_port);  // Actively open, Send SYN and ISN to peer, CLOSED->SYN_SENT
         void listen();  // Passively open, wait SYN and ISN,CLOSED->SYN_RCVD
         Socket accept();   // send SYN and ACK to peer, SYN_RCVD->ESTABLISHED
         void disconnect();  // ESTABLISHED->FIN_WAIT_1,FIN_WAIT_2,CLOSE_WAIT,LAST_ACK,TIME_WAIT->CLOSE
+        std::string recv_pkg();
+        void send_pkg(const std::string& data);
     };
 }
 
