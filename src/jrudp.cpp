52eaa21003e7021c54cb5f0997956cbbe92c3612
B777B2056 <1937343022@qq.com>
2022-01-11 16:19:12 +0800

    20220111:Timeout retransmit

--- src/jrudp.cpp
+++ src/jrudp.cpp
@@ -4,7 +4,7 @@ std::string jrReliableUDP::error_msg(std::string msg) {
     return msg + ":" + strerror(errno);
 }
 
-jrReliableUDP::Socket::Socket() : cur_seq_num(init_seq_num()), cur_ack_num(0),
+jrReliableUDP::Socket::Socket() : rto(RTO_INIT, -1, -1), cur_seq_num(init_seq_num()), cur_ack_num(0),
                                   cur_win_size(flow_win_size()), cur_state(CLOSED), dupack_cnt(1) {
     sockfd = ::socket(AF_INET, SOCK_DGRAM, 0);
     if(-1 == sockfd) {
@@ -12,9 +12,9 @@ jrReliableUDP::Socket::Socket() : cur_seq_num(init_seq_num()), cur_ack_num(0),
     }
 }
 
-jrReliableUDP::Socket::Socket(int fd, bool is_passive_end, sockaddr_in peer_addr,
+jrReliableUDP::Socket::Socket(int fd, bool is_passive_end, sockaddr_in peer_addr, RTO_Calc rto,
                               uint32_t csn, uint32_t can, ConnectionState cs)
-    : sockfd(fd), is_passive_end(is_passive_end), addr(peer_addr),
+    : sockfd(fd), is_passive_end(is_passive_end), addr(peer_addr), rto(rto),
       cur_seq_num(csn), cur_ack_num(can), cur_win_size(flow_win_size()), cur_state(cs), dupack_cnt(1) {}
 
 jrReliableUDP::Socket::~Socket() {
@@ -31,10 +31,42 @@ uint16_t jrReliableUDP::Socket::flow_win_size() const {
     return 8;
 }
 
-void jrReliableUDP::Socket::set_timeout(uint ms) {
+int64_t jrReliableUDP::Socket::get_time_diff_from_now_ms(int64_t start) const {
+    auto end = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now().time_since_epoch());
+    return std::chrono::duration_cast<std::chrono::milliseconds>(end - std::chrono::duration<int64_t, std::milli>(start)).count();
+}
+
+void jrReliableUDP::Socket::disconnect_exception(std::string msg) {
+    ::close(sockfd);
+    cur_state = CLOSED;
+    throw std::runtime_error(msg);
+}
+
+void jrReliableUDP::Socket::update_RTO_ms(int64_t rtts) {
+    rto.backoff_factor = 1;
+    if(rto.srtt == -1) {
+        rto.srtt = rtts;
+        rto.rttvar = rtts/2;
+    } else {
+        int64_t g = (2 >> RTO_G_INDEX);
+        int64_t h = (2 >> RTO_H_INDEX);
+        rto.srtt = (1 - g) * rto.srtt + g * rtts;
+        rto.rttvar = (1 - h) * rto.rttvar + h * std::abs(rtts - rto.srtt);
+    }
+    rto.RTO_ms = rto.srtt + 4 * rto.rttvar;
+}
+
+void jrReliableUDP::Socket::set_timeout() {
   timeval tv;
   tv.tv_sec = 0;
-  tv.tv_usec = ms*1000;
+  tv.tv_usec = rto.backoff_factor*rto.RTO_ms*1000;
+  ::setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
+}
+
+void jrReliableUDP::Socket::cancel_timeout() {
+  timeval tv;
+  tv.tv_sec = 0;
+  tv.tv_usec = 0;
   ::setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
 }
 
@@ -59,16 +91,18 @@ void jrReliableUDP::Socket::set_peer_address(std::string ip, uint16_t port) {
     addr.sin_port = htons(port);		// target process port number
 }
 
-void jrReliableUDP::Socket::send_raw_packet(const RawPacket& pkg) {
+void jrReliableUDP::Socket::send_raw_packet(RawPacket& pkg) {
     char buf[sizeof(RawPacket)];
+    pkg.timestamp = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now().time_since_epoch()).count();
     ::memmove(buf, &pkg, sizeof(RawPacket));
     if(-1 == ::sendto(sockfd, buf, sizeof(RawPacket), 0, reinterpret_cast<sockaddr*>(&addr), sizeof(addr))) {
         throw std::runtime_error(error_msg("Send error:"));
     }
+    if(pkg.type == ACK) { return ; }  // ACK doesn't need retransmit
+    // Add into sent buffer
     if(sent_pkgs.find(pkg.seq_num) == sent_pkgs.end()) {
         sent_pkgs[pkg.seq_num] = pkg;
     }
-    if(pkg.type == ACK) { return ; }  // ACK doesn't need retransmit
     // Wait ACK and Retransmit function
     socklen_t len = sizeof(sockaddr_in);
     RawPacket ack_pkg;
@@ -78,11 +112,18 @@ void jrReliableUDP::Socket::send_raw_packet(const RawPacket& pkg) {
     std::cout << "Sent SEQ:" << cur_seq_num << ",";
 #endif
     ++cur_seq_num;
+    set_timeout();
     if(::recvfrom(sockfd, buf, sizeof(RawPacket), 0, reinterpret_cast<sockaddr*>(&addr), &len) > 0) {
         ::memmove(&ack_pkg, buf, sizeof(RawPacket));
         if(IS_ACK(ack_pkg.type)) {
+            int64_t rtt = get_time_diff_from_now_ms(ack_pkg.timestamp);
+            update_RTO_ms(rtt);
 #ifdef DEBUG
-            std::cout << "Received ACK:" << ack_pkg.ack_num << std::endl;
+            std::cout << "Received ACK:" << ack_pkg.ack_num << ",";
+            std::cout << "RTT=" << rtt << "ms" << ",";
+            std::cout << "SRTT=" << rto.srtt << ",";
+            std::cout << "RTTVAR=" << rto.rttvar << ",";
+            std::cout << "RTO=" << rto.RTO_ms << "ms" << std::endl;
 #endif
             // ACK arrived
             if(ack_pkg.ack_num >= cur_seq_num) {
@@ -107,16 +148,20 @@ void jrReliableUDP::Socket::send_raw_packet(const RawPacket& pkg) {
             }
         } else {
             // RST arrived
-            ::close(sockfd);
-            throw std::runtime_error("Connection closed by peer.");
+            disconnect_exception("Connection closed by peer.");
         }
     } else {
         if(errno == EAGAIN) {
+            // If the waiting time exceeds the upper limit of the timeout, the current end considers that the peer end is closed
+            if(rto.RTO_ms > MAX_WAIT_TIME) {
+                disconnect_exception("Connection closed by peer.");
+            }
+            // Backoff
+            rto.backoff_factor *= 2;
             // Recv ACK timeout, retransmit current pkg
             send_raw_packet(pkg);
         } else {
-            ::close(sockfd);
-            throw std::runtime_error(error_msg("Send failed"));
+            disconnect_exception(error_msg("Send failed"));
         }
     }
 }
@@ -132,40 +177,70 @@ void jrReliableUDP::Socket::send_raw_packet(uint type) {
 }
 
 jrReliableUDP::RawPacket jrReliableUDP::Socket::wait_raw_packet() {
-    socklen_t len = sizeof(addr);
+    socklen_t len = sizeof(sockaddr_in);
     RawPacket pkg;
     char buf[sizeof(RawPacket)];
-#ifdef DEBUG
+#if (defined (FAST_TRANSMIT_DEBUG)) || (defined (TIMEOUT_TRANSMIT_DEBUG))
     int drop_cnt = 0;
 #endif
 PKG_MISSING:
     ::memset(&addr, 0, len);
+    if(cur_state == LISTEN || cur_state == SYN_SENT) {
+        cancel_timeout();
+    } else {
+        set_timeout();
+    }
     if(::recvfrom(sockfd, buf, sizeof(RawPacket), 0, reinterpret_cast<sockaddr*>(&addr), &len) > 0) {
         ::memmove(&pkg, buf, sizeof(RawPacket));
-#ifdef DEBUG
+        int64_t rtt = get_time_diff_from_now_ms(pkg.timestamp);
+        update_RTO_ms(rtt);
+#ifdef FAST_TRANSMIT_DEBUG
         if(pkg.seq_num == 2) {
             ++drop_cnt;
             if(drop_cnt == 1) {
                 goto RETRANSMIT;
             }
         }
+#endif
+
+#ifdef TIMEOUT_TRANSMIT_DEBUG
+        if(pkg.seq_num == 2) {
+            ++drop_cnt;
+            if(drop_cnt == 1) {
+                ::sleep(5);
+            }
+        }
+#endif
+
+#ifdef DEBUG
         std::cout << states[cur_state] << ",";
         std::cout << "Received SEQ:" << pkg.seq_num << ",";
 #endif
+
         if(cur_ack_num == pkg.seq_num) {
             cur_ack_num = pkg.seq_num + 1;
             rcvd_pkgs[pkg.seq_num] = pkg;
             send_raw_packet(ACK);
+
 #ifdef DEBUG
-        std::cout << "Sent ACK:" << cur_ack_num << std::endl;
+        std::cout << "Sent ACK:" << cur_ack_num << ",";
+        std::cout << "RTT=" << rtt << "ms" << ",";
+        std::cout << "SRTT=" << rto.srtt << ",";
+        std::cout << "RTTVAR=" << rto.rttvar << ",";
+        std::cout << "RTO=" << rto.RTO_ms << "ms" << std::endl;
 #endif
+
         } else if(cur_ack_num < pkg.seq_num) {
-#ifdef DEBUG
+#ifdef FAST_TRANSMIT_DEBUG
 RETRANSMIT:
 #endif
             send_raw_packet(ACK);
 #ifdef DEBUG
-        std::cout << "Sent ACK:" << cur_ack_num << std::endl;
+        std::cout << "Sent ACK:" << cur_ack_num << ",";
+        std::cout << "RTT=" << rtt << "ms" << ",";
+        std::cout << "SRTT=" << rto.srtt << ",";
+        std::cout << "RTTVAR=" << rto.rttvar << ",";
+        std::cout << "RTO=" << rto.RTO_ms << "ms" << std::endl;
 #endif
             goto PKG_MISSING;
         }
@@ -173,22 +248,24 @@ RETRANSMIT:
         rcvd_pkgs.erase(rcvd_pkgs.begin());
         return ret;
     } else {
-        ::close(sockfd);
-        throw std::runtime_error(error_msg("Recv failed"));
+        if(errno == EAGAIN) {
+            disconnect_exception("Connection closed by peer.");
+        } else {
+            disconnect_exception(error_msg("Recv failed"));
+        }
     }
 }
 
 void jrReliableUDP::Socket::bind(uint16_t port) {
     set_local_address(port);
     if(-1 == ::bind(sockfd, reinterpret_cast<sockaddr*>(&addr), sizeof(sockaddr_in))) {
-        ::close(sockfd);
-        throw std::runtime_error(error_msg("Bind failed"));
+        disconnect_exception(error_msg("Bind failed"));
     }
     this->port = port;
 }
 
 void jrReliableUDP::Socket::connect(std::string peer_ip, uint16_t peer_port) {
-    set_timeout(RTT_INIT);
+    set_timeout();
     is_passive_end = false;
     while(true) {
         switch(cur_state) {
@@ -212,7 +289,7 @@ void jrReliableUDP::Socket::connect(std::string peer_ip, uint16_t peer_port) {
         case ESTABLISHED:
             return ;
         default:
-            throw std::runtime_error("Connection established");
+            return ;
         }
     }
 }
@@ -224,7 +301,7 @@ void jrReliableUDP::Socket::listen() {
         cur_state = LISTEN;
         break;
     case LISTEN:
-        throw std::runtime_error("Already listening");
+        break;
     default:
         break;
     }
@@ -245,7 +322,7 @@ jrReliableUDP::Socket jrReliableUDP::Socket::accept() {
             } else {
                 // Received other type packet, send RST
                 send_raw_packet(RST);
-                throw std::runtime_error("Connection invalid");
+                disconnect_exception("Connection invalid");
             }
         }
             break;
@@ -263,7 +340,7 @@ jrReliableUDP::Socket jrReliableUDP::Socket::accept() {
             break;
         }
     }
-    return Socket(::dup(sockfd), true, addr, cur_seq_num, cur_ack_num, ESTABLISHED);
+    return Socket(::dup(sockfd), true, addr, rto, cur_seq_num, cur_ack_num, ESTABLISHED);
 }
 
 void jrReliableUDP::Socket::disconnect() {
@@ -297,7 +374,7 @@ void jrReliableUDP::Socket::disconnect() {
 #endif
                 return ;
             default:
-                throw std::runtime_error("Not connecting");
+                return ;
             }
         }
     } else {
@@ -331,7 +408,7 @@ void jrReliableUDP::Socket::disconnect() {
 #endif
                 return ;
             default:
-                throw std::runtime_error("Not connecting");
+                return ;
             }
         }
     }
@@ -339,14 +416,14 @@ void jrReliableUDP::Socket::disconnect() {
 
 std::string jrReliableUDP::Socket::recv_pkg() {
     if(cur_state != ESTABLISHED) {
-        throw std::runtime_error("Connection is not ESTABLISHED");
+        disconnect_exception("Connection is not ESTABLISHED");
     }
     return std::string(wait_raw_packet().data);
 }
 
 void jrReliableUDP::Socket::send_pkg(const std::string& data) {
     if(cur_state != ESTABLISHED) {
-        throw std::runtime_error("Connection is not ESTABLISHED");
+        disconnect_exception("Connection is not ESTABLISHED");
     }
     if(data.size() > MAX_SIZE) {
         throw std::runtime_error("Package too large");
