6807784259be9b86eee003fcc57e55ab2978520c
B777B2056 <1937343022@qq.com>
2022-01-12 22:43:48 +0800

    20220112:Flow control sliding window.

--- test/server/main.cpp
+++ test/server/main.cpp
@@ -7,8 +7,6 @@ int main() {
     listen.bind(8888);
     listen.listen();
     Socket server = listen.accept();
-    for(int i = 0; i < 10; ++i) {
-        server.recv_pkg();
-    }
+    while(!server.recv_pkg().empty());
     server.disconnect();
 }
